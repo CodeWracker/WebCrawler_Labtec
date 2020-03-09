044a5df71cd5c211da7102a5f217bf2aedf99d30
CodeWracker <rodrigo.souza289@etec.sp.gov.brm>
2020-03-09 20:12:08 -0300

    corrigido o problema de não fazer a verificaçaõ

--- Selenium/Processador/main.py
+++ Selenium/Processador/main.py
@@ -9,17 +9,17 @@ from download import *
 #import glob
 #from getInfos import *
 escolha = input("Selecione 1 para CPU, 2 para RAM e 3 para GPU (não implementado):   ")
-if escolha == 1:
+if int(escolha) == 1:
     url = 'https://www.cpubenchmark.net/cpu_list.php'
     pontuacaoMin = input("Qual a pontuacao minima? (escreva apenas o numero)   ")
     clockMin = input("Qual clock minimo? (escreva apenas o numero) ")
     nucleosMin = input("Qual a quantidade de nucleos minima? (escreva apenas o numero)   ")
     tdpMin = input("Qual a tdp minima? (escreva apenas o numero)    ")
-if escolha==2:
+if int(escolha)==2:
     url = 'https://www.memorybenchmark.net/ram_list.php'
     latenciaCorte = input("Qual o minimo de latencia? (escreva apenas o numero em ns)   ")
     capMin = input("Qual o minimo de capacidade? (escreva apenas o numero em GB)   ")
-if escolha == 3:
+if int(escolha) == 3:
     url = "https://www.videocardbenchmark.net/gpu_list.php"
     pontuacaoMin = input("Qual a pontuacao minima? (escreva apenas o numero)   ")
     clockMin = input("Qual clock minimo? (escreva apenas o numero) ")
@@ -29,7 +29,7 @@ print("[main.py] url: "+url)
 #createDir('cpus.csv')
 driver_url = logIn(url)
 data1t = getPerfil(driver_url)
-if escolha == 1:
+if int(escolha) == 1:
     getLinkCPU(data1t,pontuacaoMin,clockMin,tdpMin,nucleosMin)
 else:
     getLinkRAM(data1t,latenciaCorte,capMin)
