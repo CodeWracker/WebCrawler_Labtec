d8d5cbf91df190a53beaadf2bc614765aca8a0b0
CodeWracker <rodrigo.souza289@etec.sp.gov.brm>
2020-03-09 19:56:51 -0300

    comecei a colocar para verificare a gpu

--- Selenium/Processador/main.py
+++ Selenium/Processador/main.py
@@ -8,17 +8,21 @@ from download import *
 #from pyexcel.cookbook import merge_all_to_a_book
 #import glob
 #from getInfos import *
-escolha = input("Selecione 1 para processadores e 2 para RAM:   ")
+escolha = input("Selecione 1 para CPU, 2 para RAM e 3 para GPU (não implementado):   ")
 if escolha == 1:
     url = 'https://www.cpubenchmark.net/cpu_list.php'
     pontuacaoMin = input("Qual a pontuacao minima? (escreva apenas o numero)   ")
     clockMin = input("Qual clock minimo? (escreva apenas o numero) ")
     nucleosMin = input("Qual a quantidade de nucleos minima? (escreva apenas o numero)   ")
     tdpMin = input("Qual a tdp minima? (escreva apenas o numero)    ")
-else:
+if escolha==2:
     url = 'https://www.memorybenchmark.net/ram_list.php'
     latenciaCorte = input("Qual o minimo de latencia? (escreva apenas o numero em ns)   ")
     capMin = input("Qual o minimo de capacidade? (escreva apenas o numero em GB)   ")
+if escolha == 3:
+    url = "https://www.videocardbenchmark.net/gpu_list.php"
+    pontuacaoMin = input("Qual a pontuacao minima? (escreva apenas o numero)   ")
+    clockMin = input("Qual clock minimo? (escreva apenas o numero) ")
     
 
 print("[main.py] url: "+url)
